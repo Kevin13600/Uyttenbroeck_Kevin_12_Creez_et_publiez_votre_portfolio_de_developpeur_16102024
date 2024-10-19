21c968275290e93f0c7ea48ebf39f4190fc51b6e
Kevin Uyttenbroeck <160156592+Kevin13600@users.noreply.github.com>
2024-10-19 09:09:38 +0200

    Nouvelle tentative

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/Uyttenbroeck_Kevin_12_Creez_et_publiez_votre_portfolio_de_developpeur_16102024/',
