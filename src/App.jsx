22f7dc80c14c547a176be83bc966ce3d2bb089b4
Kevin Uyttenbroeck <160156592+Kevin13600@users.noreply.github.com>
2024-10-26 21:06:02 +0200

    ajout image OG pour les réseaux sociaux

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import React from "react";
-import { Helmet, HelmetProvider } from "react-helmet-async";
-import styled, { createGlobalStyle } from "styled-components";
-import HeaderHero from "./components/HeaderHero";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Works from "./components/Works";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-
-const BASE_URL =
-  "https://kevin13600.github.io/Uyttenbroeck_Kevin_12_Creez_et_publiez_votre_portfolio_de_developpeur_16102024";
+import React from 'react';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import styled, { createGlobalStyle } from 'styled-components';
+import HeaderHero from './components/HeaderHero';
+import About from './components/About';
+import Skills from './components/Skills';
+import Works from './components/Works';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -65,21 +62,15 @@ const App = () => {
       <>
         <Helmet>
           <title>Kevin Uyttenbroeck | Développeur Front-end React</title>
-          <meta
-            name="description"
-            content="Portfolio de développeur web spécialisé en React, découvrez mes projets et réalisations en intégration web et développement front-end."
-          />
+          <meta name="description" content="Portfolio de développeur web spécialisé en React, découvrez mes projets et réalisations en intégration web et développement front-end." />
           <meta name="keywords" content="développeur web, front-end, react, javascript, portfolio" />
-
+          
           {/* Open Graph / Facebook */}
           <meta property="og:type" content="website" />
-          <meta property="og:url" content={`${BASE_URL}/`} />
+          <meta property="og:url" content="https://kevin13600.github.io/Uyttenbroeck_Kevin_12_Creez_et_publiez_votre_portfolio_de_developpeur_16102024/" />
           <meta property="og:title" content="Uyttenbroeck Kevin - Portfolio Développeur Front-end" />
-          <meta
-            property="og:description"
-            content="Découvrez mes projets et réalisations en développement web front-end."
-          />
-          <meta property="og:image" content={`${BASE_URL}/assets/OG.PNG`} />
+          <meta property="og:description" content="Découvrez mes projets et réalisations en développement web front-end." />
+          <meta property="og:image" content="https://kevin13600.github.io/Uyttenbroeck_Kevin_12_Creez_et_publiez_votre_portfolio_de_developpeur_16102024/assets/OG.PNG" />
           <meta property="og:image:type" content="image/png" />
           <meta property="og:image:width" content="1200" />
           <meta property="og:image:height" content="630" />
@@ -87,13 +78,10 @@ const App = () => {
 
           {/* Twitter */}
           <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:url" content={`${BASE_URL}/`} />
+          <meta property="twitter:url" content="https://kevin13600.github.io/Uyttenbroeck_Kevin_12_Creez_et_publiez_votre_portfolio_de_developpeur_16102024/" />
           <meta property="twitter:title" content="Uyttenbroeck Kevin - Portfolio Développeur Front-end" />
-          <meta
-            property="twitter:description"
-            content="Découvrez mes projets et réalisations en développement web front-end."
-          />
-          <meta property="twitter:image" content={`${BASE_URL}/assets/OG.PNG`} />
+          <meta property="twitter:description" content="Découvrez mes projets et réalisations en développement web front-end." />
+          <meta property="twitter:image" content="https://kevin13600.github.io/Uyttenbroeck_Kevin_12_Creez_et_publiez_votre_portfolio_de_developpeur_16102024/assets/OG.PNG" />
 
           {/* Autres meta tags importants */}
           <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
@@ -103,33 +91,33 @@ const App = () => {
           <meta http-equiv="X-UA-Compatible" content="IE=edge" />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
-          <link rel="canonical" href={`${BASE_URL}/`} />
-
+          <link rel="canonical" href="https://kevin13600.github.io/Uyttenbroeck_Kevin_12_Creez_et_publiez_votre_portfolio_de_developpeur_16102024/" />
+          
           {/* Préconnexions pour améliorer les performances */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-
+          
           {/* Structured Data / Schema.org */}
           <script type="application/ld+json">
             {`
-      {
-      "@context": "http://schema.org",
-      "@type": "Person",
-      "name": "Uyttenbroeck Kevin",
-      "url": "${BASE_URL}/",
-      "jobTitle": "Développeur Front-end",
-      "description": "Développeur web spécialisé en React et technologies front-end modernes",
-      "skills": ["React", "JavaScript", "HTML5", "CSS3", "Sass", "Node.js", "MongoDB"],
-      "image": "${BASE_URL}/assets/OG.PNG",
-      "sameAs": [
-        "https://www.linkedin.com/in/kevin-uyttenbroeck-640934107/",
-        "https://github.com/Kevin13600"
-      ]
-    }
-  `}
+              {
+                "@context": "http://schema.org",
+                "@type": "Person",
+                "name": "Uyttenbroeck Kevin",
+                "url": "https://kevin13600.github.io/Uyttenbroeck_Kevin_12_Creez_et_publiez_votre_portfolio_de_developpeur_16102024/",
+                "jobTitle": "Développeur Front-end",
+                "description": "Développeur web spécialisé en React et technologies front-end modernes",
+                "skills": ["React", "JavaScript", "HTML5", "CSS3", "Sass", "Node.js", "MongoDB"],
+                "image": "https://kevin13600.github.io/Uyttenbroeck_Kevin_12_Creez_et_publiez_votre_portfolio_de_developpeur_16102024/assets/OG.PNG",
+                "sameAs": [
+                  "https://www.linkedin.com/in/kevin-uyttenbroeck-640934107/",
+                  "https://github.com/Kevin13600"
+                ]
+              }
+            `}
           </script>
         </Helmet>
-
+        
         <GlobalStyle />
         <AppWrapper>
           <header role="banner">
@@ -137,7 +125,7 @@ const App = () => {
               <HeaderHero />
             </Section>
           </header>
-
+          
           <MainContent role="main">
             <Section id="about" aria-label="À propos">
               <About />
@@ -152,7 +140,7 @@ const App = () => {
               <Contact />
             </Section>
           </MainContent>
-
+          
           <FooterSection id="footer" role="contentinfo">
             <Footer />
           </FooterSection>
