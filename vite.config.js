448e6e338c1fc5cff3fc903efc5a671d34a7c27b
Kevin Uyttenbroeck <160156592+Kevin13600@users.noreply.github.com>
2024-10-28 14:49:45 +0100

    Edition du sitemap.xml

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   plugins: [
-    react(),
-    sitemap({
-      hostname: 'https://kevin13600.github.io',
-      urls: [
-        '/Uyttenbroeck_Kevin_12_Creez_et_publiez_votre_portfolio_de_developpeur_16102024/',
-        '/Uyttenbroeck_Kevin_12_Creez_et_publiez_votre_portfolio_de_developpeur_16102024/#about',
-        '/Uyttenbroeck_Kevin_12_Creez_et_publiez_votre_portfolio_de_developpeur_16102024/#skills',
-        '/Uyttenbroeck_Kevin_12_Creez_et_publiez_votre_portfolio_de_developpeur_16102024/#works',
-        '/Uyttenbroeck_Kevin_12_Creez_et_publiez_votre_portfolio_de_developpeur_16102024/#contact'
-      ]
-    })
+    react()
   ],
   base: '/Uyttenbroeck_Kevin_12_Creez_et_publiez_votre_portfolio_de_developpeur_16102024/',
 })
